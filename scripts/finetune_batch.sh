b55a02b3ccccb2ee4f9cc85d8458c90959246ad4
xinyunsunshine <xinyunsunshine@gmail.com>
2025-10-22 11:29:43 -0400

    finetune for all collected rollouts in a loop

---
+++ scripts/finetune_batch.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# chmod +x train_eval.sh
+# ./train_eval.sh
+# Ensure the script stops on error
+set -e
+
+# Define the command with parameters
+CONFIG_DIR="config"
+CONFIG_NAME="dp_square_mh_sft.yaml"
+SEED=42
+DEVICE="cuda:0"
+EPOCH_NUM=30
+CKPT_EVERY=5
+VAL_EVERY=10
+MAX_TRAIN_EPISODES=300
+
+# HYDRA_RUN_DIR="data/outputs/$(date +%Y.%m.%d)/$(date +%H.%M.%S)_lr${LR}_sigma${X_SIGMA}_k${k}" #the old dir name
+PRETRAINED_CKPT_PATH="'data/outputs/square/pre_train/checkpoints/epoch=0750-test_mean_score=0.440.ckpt'"
+DATAPATH="data/new_demos/square_mh_policy_demos_test.hdf5"
+
+# Overwrite flag (set to true to re-run evaluation)
+OVERWRITE=false
+
+# Loop through episode indices 0 to 49
+for x in {0..49}; do
+    HYDRA_RUN_DIR="data/outputs/square/ftall0/sft${x}"
+    LOG_NAME="square_training_all_sft${x}"
+
+    echo "Training episode index: $x"
+    echo "Output directory: $HYDRA_RUN_DIR"
+    echo "Log name: $LOG_NAME"
+
+    # Run the training script
+    python train.py \
+        --config-dir=$CONFIG_DIR \
+        --config-name=$CONFIG_NAME \
+        training.finetune=true \
+        training.pretrained_ckpt_path=$PRETRAINED_CKPT_PATH \
+        training.seed=$SEED \
+        training.device=$DEVICE \
+        logging.name="$LOG_NAME" \
+        hydra.run.dir="$HYDRA_RUN_DIR"\
+        training.num_epochs=$EPOCH_NUM \
+        task.dataset.max_train_episodes=$MAX_TRAIN_EPISODES \
+        training.val_every=$VAL_EVERY\
+        training.checkpoint_every=$CKPT_EVERY\
+        training.rollout_every=1\
+        task.dataset.dataset_path=$DATAPATH\
+        task.dataset.finetune=true\
+        task.dataset.episode_indices="[$x]"
+
+    # Save the directory path
+    echo "$HYDRA_RUN_DIR" > last_run_dir.txt
+
+done
